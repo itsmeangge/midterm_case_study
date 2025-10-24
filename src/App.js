a0499a4e8a15feb79af5c498b24c4c118b500f4d
PauloEmpizo <166630217+PauloEmpizo@users.noreply.github.com>
2025-10-24 20:23:56 +0800

    Product-pages added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Homepage from "./components/Homepage";
-import ProductList from "./components/ProductList";
-import ProductDetails from "./components/ProductDetails";
-import Cart from "./components/Cart";
-import Checkout from "./components/Checkout";
+import HomePage from "./pages/HomePage";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/products" element={<ProductList />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import homepageBg from "../assets/homepage-bg.jpg";
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${homepageBg})`,
+        backgroundSize: "cover",           
+        backgroundPosition: "center",      
+        backgroundRepeat: "no-repeat",     
+        height: "90vh",                   
+        width: "100%",                    
+        display: "flex",
+        alignItems: "center",              
+        justifyContent: "flex-start",      
+        color: "white",
+        textShadow: "0 2px 6px rgba(0,0,0,0.7)",
+        paddingLeft: "60px",               
+        boxSizing: "border-box",
+      }}
+    >
+      <div style={{ maxWidth: "500px", textAlign: "left" }}>
+        <h2 style={{ fontSize: "1.9rem", lineHeight: "1.5" }}>
+          Redefine your wardrobe with timeless pieces from WearDistrict.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
