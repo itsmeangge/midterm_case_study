d44aa36aa3edf1d33247b4fab9fcd7b6a66e6111
Potchibels <johnmichaelgigante26@gmail.com>
2025-10-25 10:57:28 +0800

    adding more product images

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,12 @@
 import shirt1 from "../designs/shirt1.jpg";
 import shirt2 from "../designs/shirt2.jpg";
 import shirt3 from "../designs/shirt3.jpg";
+import shirt4 from "../designs/shirt4.jpg";
+import shirt5 from "../designs/shirt5.jpg";
+import shirt6 from "../designs/shirt6.jpg";
+import shirt7 from "../designs/shirt7.jpg";
+import shirt8 from "../designs/shirt8.jpg";
+import shirt9 from "../designs/shirt9.jpg";
 
 const products = [
   {
@@ -23,6 +29,49 @@ const products = [
     price: 599, 
     image: shirt3, 
     description: "Classic white v-neck jersey with dark trim and logo. Lightweight and comfortable, offering a sporty look for everyday activities."
+  },
+  {
+    id: 4,
+    name: "Sans Emploi", 
+    price: 699, 
+    image: shirt4,
+    description: "Pink and white striped jersey featuring a bold 'California' print and soft, breathable fabric. Perfect for a relaxed streetwear look. Available in sizes S to XL."
+  },
+  {
+    id: 5,
+    name: "California Shirt", 
+    price: 849, 
+    image: shirt5,
+    description: "Pink and white striped jersey featuring a bold 'California' print and soft, breathable fabric. Perfect for a relaxed streetwear look. Available in sizes S to XL."
+
+  },
+  {
+    id: 6,
+    name: "Seoul Shirt", 
+    price: 899, 
+    image: shirt6,
+    description: "Royal blue and white panel jersey with 'SEOUL NINETYNINE' print. Athletic-inspired design with breathable mesh side panels. Available in sizes S to XL."
+  },
+  {
+    id: 7,
+    name: "Speed Shirt", 
+    price: 1999, 
+    image: shirt7,
+    description: "Black and white performance jersey featuring bold 'SPEED' graphics and 'stay wild' accent. Built for comfort and movement. Available in sizes S to XL."
+  },
+  {
+    id: 8,
+    name: "Standard Track Shirt", 
+    price: 999, 
+    image: shirt8,
+    description: "Pink and black polo-style jersey with 'STANDARD TRACK' logo. Combines athletic design with a classic collar finish. Available in sizes S to XL."
+  },
+  {
+    id: 9,
+    name: "Rose Martine Limited Edition", 
+    price: 1299, 
+    image: shirt9,
+    description: "Light blue and black color-block jersey with 'ROSE MARTINE' text detail. A stylish sporty piece crafted for comfort and modern aesthetics. Available in sizes S to XL."
   }
 ];
 
